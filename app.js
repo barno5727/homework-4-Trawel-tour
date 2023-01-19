1f77ac027ec809a758e8a1aa2577678d0efacceb
unknown <barnoodilovna7405727@mail.ru>
2023-01-20 02:23:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let $ = 10650
+let  euro = 11650
+
+ticket => 500 
+living => 250 
+tour => 120
+
+alert("Travel the world with our tour and enjoy!!!")
+let sum = prompt("So'm kiriting!")
+
+// let xarajat = ticket * dollar + living * dollar + tour * euro
+
+
+if(sum >= 1260) {
+    console.log("oq yol")
+}
+
+else if (sum < 1260) {
+    console.log("sizda pul yetarli emas")
+}
+       
+else {
+    console.log("hato")
+}
